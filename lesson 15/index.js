b6ccd7d2079e9753a8b121c0c20101c0f261f723
Denis Markin <denis.markin.95@gmail.com>
2023-03-27 17:36:16 +0400

    lesson15_pre

---
+++ lesson 15/index.js
@@ -0,0 +1,35 @@
+// Task 1
+let x = 20;
+let y = 58;
+let z = 42;
+console.log(x + y + z);
+
+// Task 2
+
+let seconds = 60;
+let hour = 60;
+let day = 24;
+let year = 365;
+const myAgeInSeconds = 27;
+console.log(myAgeInSeconds * year * day * hour * seconds);
+
+// Task 4 ?
+let a = "1";
+let b = "2";
+let c = "белых медведей";
+console.log(a + b, c);
+
+// Task 3
+
+let count = 42;
+const username = `${count}`;
+console.log(username);
+console.log(count);
+
+// ADVANCED task 1
+
+let k = "4";
+k = "3";
+let j = "3";
+j = "4";
+console.log(k, j);
