d0c0bae72553643b55eb23e7374bf5416cd5dd97
Denis Markin <denis.markin.95@gmail.com>
2023-03-28 15:55:22 +0400

    lesson15-completion

--- lesson 15/index.js
+++ lesson 15/index.js
@@ -13,23 +13,47 @@ let year = 365;
 const myAgeInSeconds = 27;
 console.log(myAgeInSeconds * year * day * hour * seconds);
 
-// Task 4 ?
-let a = "1";
-let b = "2";
-let c = "белых медведей";
-console.log(a + b, c);
-
 // Task 3
-
 let count = 42;
 const username = `${count}`;
 console.log(username);
 console.log(count);
+// Second way
+let qw = "42";
+console.log(qw, +qw);
+
+// Task 4 ?
+let a = 1;
+let b = 2;
+let c = "белых медведей";
+console.log(`${a}` + `${b}`, c);
+
+// Task 5
+let first = "доступ";
+let second = "морпех";
+let third = "наледь";
+let fourth = "попрек";
+let fifth = "рубило";
+let lengthWords_1 = first.length;
+let lengthWords_2 = second.length;
+let lengthWords_3 = third.length;
+let lengthWords_4 = fourth.length;
+let lengthWords_5 = fifth.length;
+console.log(
+  lengthWords_1 + lengthWords_2 + lengthWords_3 + lengthWords_4 + lengthWords_5
+);
+// я что-то тут намудрил, но всё работает, слова посчитаны :)
+
+Task 6
+let name_user = prompt("Enter your name");
+let age_user = prompt("Enter your age");
+console.log(name_user);
+console.log(age_user);
 
 // ADVANCED task 1
 
-let k = "4";
-k = "3";
-let j = "3";
-j = "4";
+let k = 4;
+k = 3;
+let j = 3;
+j = 4;
 console.log(k, j);
